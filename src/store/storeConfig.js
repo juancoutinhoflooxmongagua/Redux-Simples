739ca21acc78d37e88137df6172040b5cbaec72a
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-07 10:47:54 -0300

    feat: create Reducer with Reducers

---
+++ src/store/storeConfig.js
@@ -0,0 +1,15 @@
+import { createStore, combineReducers } from 'redux';
+
+const reducers = combineReducers({
+  numbers: function (state = { min: 1, max: 10 }, action) {
+    switch (action.type) {
+ 
+        default:
+        return state;
+    }
+  }
+});
+
+const store = createStore(reducers);
+
+export default store;
