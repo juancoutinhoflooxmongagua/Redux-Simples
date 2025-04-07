6cd398db6dd240ae8fb490bf9df9fc87eb419b21
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-07 10:52:12 -0300

    feat: update functions off Reducer in Index.js

--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -3,8 +3,7 @@ import { createStore, combineReducers } from 'redux';
 const reducers = combineReducers({
   numbers: function (state = { min: 1, max: 10 }, action) {
     switch (action.type) {
- 
-        default:
+      default:
         return state;
     }
   }
